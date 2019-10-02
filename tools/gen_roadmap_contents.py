828502a94172dabb881a7363c1989f864557ce1b
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-10-02 14:06:32 +0400

    Add validation for README.md

--- tools/gen_roadmap_contents.py
+++ tools/gen_roadmap_contents.py
@@ -13,45 +13,45 @@ import json
 import lxml.etree as et
 
 # Constants
-CONTENTS_BEGIN = '<!--roadmap.mm table of contents begin-->'
-CONTENTS_END = '<!--roadmap.mm table of contents end-->'
+CONTENTS_BEGIN = "<!--roadmap.mm table of contents begin-->"
+CONTENTS_END = "<!--roadmap.mm table of contents end-->"
 
-RELATIVE_README_FILEPATH = '../README.md'
-RELATIVE_ROADMAP_FILEPATH = '../roadmap.mm'
-RELATIVE_LINKS_FILEPATH = 'links.json'
-RELATIVE_ALTERNATIVE_NAMES_FILEPATH = 'alternative_names.json'
+RELATIVE_README_FILEPATH = "../README.md"
+RELATIVE_ROADMAP_FILEPATH = "../roadmap.mm"
+RELATIVE_LINKS_FILEPATH = "links.json"
+RELATIVE_ALTERNATIVE_NAMES_FILEPATH = "alternative_names.json"
 
 README_FILEPATH = os.path.abspath(RELATIVE_README_FILEPATH)
 ROADMAP_FILEPATH = os.path.abspath(RELATIVE_ROADMAP_FILEPATH)
 LINKS_FILEPATH = os.path.abspath(RELATIVE_LINKS_FILEPATH)
 ALTERNATIVE_NAMES_FILEPATH = os.path.abspath(RELATIVE_ALTERNATIVE_NAMES_FILEPATH)
 
-LINKS = json.loads(open(LINKS_FILEPATH, 'r').read())
-ALTERNATIVE_NAMES = json.loads(open(ALTERNATIVE_NAMES_FILEPATH, 'r').read())
+LINKS = json.loads(open(LINKS_FILEPATH, "r").read())
+ALTERNATIVE_NAMES = json.loads(open(ALTERNATIVE_NAMES_FILEPATH, "r").read())
 
 
 def get_tree_text(node, level=-2):
     result = []
-    tab = '    '
-    text = node.get('TEXT')
+    tab = "    "
+    text = node.get("TEXT")
     link = LINKS.get(text)
     text = ALTERNATIVE_NAMES.get(text, text)
     if text:
         if level == -2:
-            result.append('\n## {}\n'.format(text))
+            result.append("\n## {}\n".format(text))
         elif level == -1:
-            result.append('\n### {}\n'.format(text))
+            result.append("\n### {}\n".format(text))
         elif link:
-            result.append('{}- [{}]({})\n'.format(tab * level, text, link))
+            result.append("{}- [{}]({})\n".format(tab * level, text, link))
         else:
-            result.append('{}- {}\n'.format(tab * level, text))
+            result.append("{}- {}\n".format(tab * level, text))
     for sub_node in node:
         result.append(get_tree_text(sub_node, level=level + 1))
-    return ''.join(result)
+    return "".join(result)
 
 
 def main():
-    with open(ROADMAP_FILEPATH, 'r') as f:
+    with open(ROADMAP_FILEPATH, "r") as f:
         xml = f.read()
         parser = et.XMLParser(strip_cdata=False)
         tree = et.fromstring(xml, parser)
@@ -59,18 +59,20 @@ def main():
         for node in tree.xpath("/map/node/node"):
             readme_contents.append(get_tree_text(node))
 
-        readme_contents_txt = ''.join(readme_contents)
+        readme_contents_txt = "".join(readme_contents)
 
-        with open(README_FILEPATH, 'r') as old_readme:
+        with open(README_FILEPATH, "r") as old_readme:
             old_readme_content = old_readme.read()
 
         begin, middle = old_readme_content.split(CONTENTS_BEGIN)
         middle, end = middle.split(CONTENTS_END)
         updated_middle = readme_contents_txt
-        updated_readme_contents = f'{begin}{CONTENTS_BEGIN}{updated_middle}{CONTENTS_END}{end}'
-        with open(README_FILEPATH, 'w') as updated_readme:
+        updated_readme_contents = "{}{}{}{}{}".format(
+            begin, CONTENTS_BEGIN, updated_middle, CONTENTS_END, end
+        )
+        with open(README_FILEPATH, "w") as updated_readme:
             updated_readme.write(updated_readme_contents)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
