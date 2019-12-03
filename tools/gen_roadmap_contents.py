170c8ff8acecdd4dacd720dfdb53aabfebcb088d
Egor Tolstoy <igrekde@gmail.com>
2019-12-03 19:30:42 +0300

    Update gen_roadmap_contents.py

--- tools/gen_roadmap_contents.py
+++ tools/gen_roadmap_contents.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-"""Update table of content in main `README.md` using data from `roadmap.mm`"""
+"""Update table of content in main `README.md` using data from `roadmap.puml`"""
 
 __author__ = "Pavel Kedrinskiy"
-__maintaner__ = "Pavel Kedrinskiy"
-
+__maintaner__ = "Pavel Kedrinskiy & Egor Tolstoy"
 
 # standard libs
 import os
@@ -20,39 +19,10 @@ CONTENTS_BEGIN = "<!--roadmap.mm table of contents begin-->"
 CONTENTS_END = "<!--roadmap.mm table of contents end-->"
 
 RELATIVE_README_FILEPATH = "../README.md"
-RELATIVE_ROADMAP_FILEPATH = "../roadmap.mm"
 RELATIVE_PUML_FILEPATH = "../roadmap.puml"
-RELATIVE_LINKS_FILEPATH = "links.json"
-RELATIVE_ALTERNATIVE_NAMES_FILEPATH = "alternative_names.json"
 
 README_FILEPATH = os.path.abspath(RELATIVE_README_FILEPATH)
-ROADMAP_FILEPATH = os.path.abspath(RELATIVE_ROADMAP_FILEPATH)
 PUML_FILEPATH = os.path.abspath(RELATIVE_PUML_FILEPATH)
-LINKS_FILEPATH = os.path.abspath(RELATIVE_LINKS_FILEPATH)
-ALTERNATIVE_NAMES_FILEPATH = os.path.abspath(RELATIVE_ALTERNATIVE_NAMES_FILEPATH)
-
-LINKS = json.loads(open(LINKS_FILEPATH, "r").read())
-ALTERNATIVE_NAMES = json.loads(open(ALTERNATIVE_NAMES_FILEPATH, "r").read())
-
-
-def get_tree_text(node, level=-2):
-    result = []
-    tab = "    "
-    text = node.get("TEXT")
-    link = LINKS.get(text)
-    text = ALTERNATIVE_NAMES.get(text, text)
-    if text:
-        if level == -2:
-            result.append("\n## {}\n".format(text))
-        elif level == -1:
-            result.append("\n### {}\n".format(text))
-        elif link:
-            result.append("{}- [{}]({})\n".format(tab * level, text, link))
-        else:
-            result.append("{}- {}\n".format(tab * level, text))
-    for sub_node in node:
-        result.append(get_tree_text(sub_node, level=level + 1))
-    return "".join(result)
 
 def main():
     with open(README_FILEPATH, "r") as old_readme:
