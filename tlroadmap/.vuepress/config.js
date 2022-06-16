4a4a3f1788ee57929ccd916bf85661a1f05c3130
Eugene Katella <katellaevgenii@gmail.com>
2022-06-16 22:25:36 +0300

    Update config.js

--- tlroadmap/.vuepress/config.js
+++ tlroadmap/.vuepress/config.js
@@ -41,8 +41,8 @@ module.exports = (ctx) => ({
     docsDir: 'tlroadmap',
     editLinks: true,
     banner: {
-      text: 'Хочешь получать новые знания про тимлидство? Подписывайся в Telegram на Techlead Good Reads!',
-      link: 'https://t.me/leadgr',
+      text: 'С 27 по 1 июля пройдет онлайн-конференция для тимлидов Podlodka Teamlead Crew!',
+      link: 'https://podlodka.io/tlcrew?utm_campaign=early_bird_tl_crew_8&utm_source=сайт&utm_medium=social&utm_content=tlroadmap',
     },
     locales: {
       '/': {
