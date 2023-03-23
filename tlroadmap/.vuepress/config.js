2c3164e0b42bf3577037ee250ab5179cfba4fbd1
Egor Tolstoy <igrekde@gmail.com>
2023-03-23 14:38:33 +0300

    Update config.js

--- tlroadmap/.vuepress/config.js
+++ tlroadmap/.vuepress/config.js
@@ -57,7 +57,7 @@ module.exports = (ctx) => ({
           { text: 'Как использовать', link: '/guide.html' },
           { text: 'Чат в Telegram', link: 'https://tlinks.run/tlbootcamp' },
           // TODO: Сейчас базовая тема использует ensureExt, если считает, что ссылка внутренняя
-          // как он понимает, что ссылка внешняя или внутренняя:
+          // как он понимает, что ссылка внешняя или внутренняя: 
           // https://github.com/vuejs/vuepress/blob/b105089d4b09c17a2085a2637cf1a7a5553c2c3f/packages/%40vuepress/theme-default/util/index.js#L4
           // после этого он делает вот это
           // https://github.com/vuejs/vuepress/blob/master/packages/%40vuepress/theme-default/components/NavLink.vue#L37-L39
